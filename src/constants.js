d58b4d492c5b9276299f12be0f0aab83c48555d1
yahav3801 <ybh00790@gmail.com>
2025-01-14 16:28:54 +0200

    added project

--- src/constants.js
+++ src/constants.js
@@ -1,287 +1,311 @@
 export const navLinks = [
-    {
-      id: 1,
-      name: 'Home',
-      href: '#home',
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Work",
+    href: "#work",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: "Emily Johnson",
+    position: "Marketing Director at GreenLeaf",
+    img: "assets/review1.png",
+    review:
+      "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+  },
+  {
+    id: 2,
+    name: "Mark Rogers",
+    position: "Founder of TechGear Shop",
+    img: "assets/review2.png",
+    review:
+      "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.",
+  },
+  {
+    id: 3,
+    name: "John Dohsas",
+    position: "Project Manager at UrbanTech ",
+    img: "assets/review3.png",
+    review:
+      "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+  },
+  {
+    id: 4,
+    name: "Ether Smith",
+    position: "CEO of BrightStar Enterprises",
+    img: "assets/review4.png",
+    review:
+      "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.",
+  },
+];
+
+export const myProjects = [
+  {
+    title: "YBH Directory",
+    desc: "YBH Directory is a platform where entrepreneurs can submit their startup ideas for virtual pitch competitions, browse other pitches, and gain exposure through a clean minimalistic design for a smooth user experience.",
+    subdesc:
+      "This project showcases the latest features of Next.js 15, including Next.js Form for seamless client-side form submissions, Unstable After for background tasks, Server Component HMR Cache for faster development, and Partial Prerendering for performance optimization.",
+    href: "https://ybh-directory.vercel.app/",
+    texture: "/textures/project/project1gif.mp4",
+    logo: "/assets/ybhdirlogo2.png",
+    logoStyle: {
+      backgroundColor: "",
+      border: "0.2px solid#d3507a",
+      boxShadow: "0px 0px 20px 0px #EE2B69",
     },
-    {
-      id: 2,
-      name: 'About',
-      href: '#about',
-    },
-    {
-      id: 3,
-      name: 'Work',
-      href: '#work',
-    },
-    {
-      id: 4,
-      name: 'Contact',
-      href: '#contact',
-    },
-  ];
-  
-  export const clientReviews = [
-    {
-      id: 1,
-      name: 'Emily Johnson',
-      position: 'Marketing Director at GreenLeaf',
-      img: 'assets/review1.png',
-      review:
-        'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
-    },
-    {
-      id: 2,
-      name: 'Mark Rogers',
-      position: 'Founder of TechGear Shop',
-      img: 'assets/review2.png',
-      review:
-        'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
-    },
-    {
-      id: 3,
-      name: 'John Dohsas',
-      position: 'Project Manager at UrbanTech ',
-      img: 'assets/review3.png',
-      review:
-        'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
-    },
-    {
-      id: 4,
-      name: 'Ether Smith',
-      position: 'CEO of BrightStar Enterprises',
-      img: 'assets/review4.png',
-      review:
-        'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
-    },
-  ];
-  
-  export const myProjects = [
-    {
-      title: 'YBH Directory',
-      desc: 'YBH Directory is a platform where entrepreneurs can submit their startup ideas for virtual pitch competitions, browse other pitches, and gain exposure through a clean minimalistic design for a smooth user experience.',
-      subdesc:
-        'This project showcases the latest features of Next.js 15, including Next.js Form for seamless client-side form submissions, Unstable After for background tasks, Server Component HMR Cache for faster development, and Partial Prerendering for performance optimization.',
-      href: 'https://ybh-directory.vercel.app/',
-      texture: '/textures/project/project1gif.mp4',
-      logo: '/assets/ybhdirlogo2.png',
-      logoStyle: {
-        backgroundColor: '',
-        border: '0.2px solid#d3507a',
-        boxShadow: '0px 0px 20px 0px #EE2B69',
+    spotlight: "/assets/spotlightybh.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/nextjswhite.png",
       },
-      spotlight: '/assets/spotlightybh.png',
-      tags: [
-        {
-          id: 1,
-          name: 'Next.js',
-          path: '/assets/nextjswhite.png',
-        },
-        {
-          id: 2,
-          name: 'TailwindCSS',
-          path: 'assets/tailwindcss.png',
-        },
-        {
-          id: 3,
-          name: 'TypeScript',
-          path: '/assets/typescript.png',
-        },
-        {
-          id: 4,
-          name: 'Framer Motion',
-          path: '/assets/Sanity-logo.png',
-        },
-      ],
-    },
-    {
-      title: 'Coming Soon',
-      desc: '...',
-      subdesc:
-        '......',
-      href: '',
-      texture: '/textures/project/Coming_Soon.mp4',
-      logo: '/assets/questionmark.png',
-      logoStyle: {
-        backgroundColor: '#13202F',
-        border: '0.2px solid #17293E',
-        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
       },
-      spotlight: '/assets/spotlight2.png',
-      tags: [
-        {
-          id: 1,
-          name: 'React.js',
-          path: '/assets/questionmark.png',
-        },
-        {
-          id: 2,
-          name: 'TailwindCSS',
-          path: '/assets/questionmark.png',
-        },
-        {
-          id: 3,
-          name: 'TypeScript',
-          path: '/assets/questionmark.png',
-        },
-        {
-          id: 4,
-          name: 'Framer Motion',
-          path: '/assets/questionmark.png',
-        },
-      ],
-    },
-    {
-      title: 'Coming Soon',
-      desc: '...',
-      subdesc:
-        '......',
-      href: '',
-      texture: '/textures/project/Coming_Soon.mp4',
-      logo: '/assets/questionmark.png',
-      logoStyle: {
-        backgroundColor: '#13202F',
-        border: '0.2px solid #17293E',
-        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
       },
-      spotlight: '/assets/spotlight2.png',
-      tags: [
-        {
-          id: 1,
-          name: 'React.js',
-          path: '/assets/questionmark.png',
-        },
-        {
-          id: 2,
-          name: 'TailwindCSS',
-          path: '/assets/questionmark.png',
-        },
-        {
-          id: 3,
-          name: 'TypeScript',
-          path: '/assets/questionmark.png',
-        },
-        {
-          id: 4,
-          name: 'Framer Motion',
-          path: '/assets/questionmark.png',
-        },
-      ],
-    },
-    {
-      title: 'Coming Soon',
-      desc: '...',
-      subdesc:
-        '......',
-      href: '',
-      texture: '/textures/project/Coming_Soon.mp4',
-      logo: '/assets/questionmark.png',
-      logoStyle: {
-        backgroundColor: '#13202F',
-        border: '0.2px solid #17293E',
-        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/Sanity-logo.png",
       },
-      spotlight: '/assets/spotlight2.png',
-      tags: [
-        {
-          id: 1,
-          name: 'React.js',
-          path: '/assets/questionmark.png',
-        },
-        {
-          id: 2,
-          name: 'TailwindCSS',
-          path: '/assets/questionmark.png',
-        },
-        {
-          id: 3,
-          name: 'TypeScript',
-          path: '/assets/questionmark.png',
-        },
-        {
-          id: 4,
-          name: 'Framer Motion',
-          path: '/assets/questionmark.png',
-        },
-      ],
-    }
-    ,
-    {
-      title: 'Imaginify - AI Photo Manipulation App',
-      desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
-      subdesc:
-        'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
-      href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
-      texture: '/textures/project/project5.mp4',
-      logo: '/assets/project-logo5.png',
-      logoStyle: {
-        backgroundColor: '#1C1A43',
-        border: '0.2px solid #252262',
-        boxShadow: '0px 0px 60px 0px #635BFF4D',
-      },
-      spotlight: '/assets/spotlight5.png',
-      tags: [
-        {
-          id: 1,
-          name: 'React.js',
-          path: '/assets/react.svg',
-        },
-        {
-          id: 2,
-          name: 'TailwindCSS',
-          path: 'assets/tailwindcss.png',
-        },
-        {
-          id: 3,
-          name: 'TypeScript',
-          path: '/assets/typescript.png',
-        },
-        {
-          id: 4,
-          name: 'Framer Motion',
-          path: '/assets/framer.png',
-        },
-      ],
+    ],
+  },
+  {
+    title: "Kindergarten Ciufit",
+    desc: "Ciufit is a Kindergarten in the heart of Kiryat Ata and it might seem like a simple landing page at first glace but, behind it lies a world of creative animations. With a focus on providing a welcoming and nurturing environment for children, the kindergarten is a haven of learning and growth. ",
+    subdesc:
+      "In this Project you will find many techniques of creating animations with GSAP. From simple to complex animations we created stunning and engaging user experiences from parallax scrolling effects to svg path animations, while maintaining a clean and childish design fitting the kindergarten theme.",
+    href: "https://www.ciufit.co.il/",
+    texture: "/textures/project/project2gif.mp4",
+    logo: "/assets/ciufitlogo.png",
+    logoStyle: {
+      backgroundColor: "#FFFFFF4D",
+      border: "0.2px solid #FFFFFF4D",
+      boxShadow: "0px 0px 60px 0px #FFFFFF4D",
     },
-  ];
-  
-  export const calculateSizes = (isSmall, isMobile, isTablet) => {
-    return {
-      deskScale: isSmall ? 0.08 : isMobile ? 0.04 : 0.07,
-      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -3.5, 0],
-      cubePosition: isSmall ? [4, -7, 0] : isMobile ? [6, -6, 0] : isTablet ? [7, -5, 0] : [16, -10, 0],
-      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [6, 5, 0] : isTablet ? [5, 5, 0] : [12, 3, 0],
-      ringPosition: isSmall ? [-4, 5, 0] : isMobile ? [-6, 6, 0] : isTablet ? [-8, 6, 0] : [-12, 5, 0],
-      targetPosition: isSmall ? [-10, -6, -10] : isMobile ? [-13, -10, -10] : isTablet ? [-15, -7, -10] : [-22, -10, -10],
-    };
+    spotlight: "/assets/spotlight5.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/nextjswhite.png",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "GSAP",
+        path: "/assets/gsap.png",
+      },
+    ],
+  },
+  {
+    title: "Coming Soon",
+    desc: "...",
+    subdesc: "......",
+    href: "",
+    texture: "/textures/project/Coming_Soon.mp4",
+    logo: "/assets/questionmark.png",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
+    },
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "",
+        path: "/assets/questionmark.png",
+      },
+      {
+        id: 2,
+        name: "",
+        path: "/assets/questionmark.png",
+      },
+      {
+        id: 3,
+        name: "",
+        path: "/assets/questionmark.png",
+      },
+      {
+        id: 4,
+        name: "",
+        path: "/assets/questionmark.png",
+      },
+    ],
+  },
+  {
+    title: "Coming Soon",
+    desc: "...",
+    subdesc: "......",
+    href: "",
+    texture: "/textures/project/Coming_Soon.mp4",
+    logo: "/assets/questionmark.png",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
+    },
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "",
+        path: "/assets/questionmark.png",
+      },
+      {
+        id: 2,
+        name: "",
+        path: "/assets/questionmark.png",
+      },
+      {
+        id: 3,
+        name: "",
+        path: "/assets/questionmark.png",
+      },
+      {
+        id: 4,
+        name: "",
+        path: "/assets/questionmark.png",
+      },
+    ],
+  },
+  {
+    title: "Imaginify - AI Photo Manipulation App",
+    desc: "Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.",
+    subdesc:
+      "Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.",
+    href: "https://www.youtube.com/watch?v=Ahwoks_dawU",
+    texture: "/textures/project/project5.mp4",
+    logo: "/assets/project-logo5.png",
+    logoStyle: {
+      backgroundColor: "#1C1A43",
+      border: "0.2px solid #252262",
+      boxShadow: "0px 0px 60px 0px #635BFF4D",
+    },
+    spotlight: "/assets/spotlight5.png",
+    tags: [
+      {
+        id: 1,
+        name: "",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.08 : isMobile ? 0.04 : 0.07,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -3.5, 0],
+    cubePosition: isSmall
+      ? [4, -7, 0]
+      : isMobile
+      ? [6, -6, 0]
+      : isTablet
+      ? [7, -5, 0]
+      : [16, -10, 0],
+    reactLogoPosition: isSmall
+      ? [3, 4, 0]
+      : isMobile
+      ? [6, 5, 0]
+      : isTablet
+      ? [5, 5, 0]
+      : [12, 3, 0],
+    ringPosition: isSmall
+      ? [-4, 5, 0]
+      : isMobile
+      ? [-6, 6, 0]
+      : isTablet
+      ? [-8, 6, 0]
+      : [-12, 5, 0],
+    targetPosition: isSmall
+      ? [-10, -6, -10]
+      : isMobile
+      ? [-13, -10, -10]
+      : isTablet
+      ? [-15, -7, -10]
+      : [-22, -10, -10],
   };
-  
-  export const workExperiences = [
-    {
-      id: 1,
-      name: 'IITC',
-      pos: 'Full Stack Student',
-      duration: '2024',
-      title: "IITC was my first step into Web Development, There i mastered the basics of web development and design for over 800 intensive and demanding hours of hands on experience.",
-      icon: '/assets/IITC-LOGO.png',
-      animation: 'victory',
-    },
-    {
-      id: 2,
-      name: 'Freelancer',
-      pos: 'Web Developer',
-      duration: '2024 - present',
-      title: "Now I'm working as a freelance web developer, creating websites and web applications for clients. I use my skills to deliver high-quality designs that meet the unique needs of each project.",
-      icon: '/assets/freelance.svg',
-      animation: 'clapping',
-    },
-    {
-      id: 3,
-      name: 'Your Problem - My next project',
-      pos: 'Looking for my next challenge',
-      duration: 'present - always',
-      title: "I'm always looking for new challenges and opportunities to learn and grow as a developer. So let me take on your problems and turn them into a stunning website.",
-      icon: '/assets/future1.png',
-      animation: 'salute',
-    },
-  ];
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: "IITC",
+    pos: "Full Stack Student",
+    duration: "2024",
+    title:
+      "IITC was my first step into Web Development, There i mastered the basics of web development and design for over 800 intensive and demanding hours of hands on experience.",
+    icon: "/assets/IITC-LOGO.png",
+    animation: "victory",
+  },
+  {
+    id: 2,
+    name: "Freelancer",
+    pos: "Web Developer",
+    duration: "2024 - present",
+    title:
+      "Now I'm working as a freelance web developer, creating websites and web applications for clients. I use my skills to deliver high-quality designs that meet the unique needs of each project.",
+    icon: "/assets/freelance.svg",
+    animation: "clapping",
+  },
+  {
+    id: 3,
+    name: "Your Problem - My next project",
+    pos: "Looking for my next challenge",
+    duration: "present - always",
+    title:
+      "I'm always looking for new challenges and opportunities to learn and grow as a developer. So let me take on your problems and turn them into a stunning website.",
+    icon: "/assets/future1.png",
+    animation: "salute",
+  },
+];
